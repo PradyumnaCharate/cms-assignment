5d0283a95c515271762ad967a3bcfaba2acb435b
unknown <pradyumnacharate1@gmail.com>
2024-11-13 11:36:45 +0530

    get by slug

--- src/posts/posts.route.js
+++ src/posts/posts.route.js
@@ -5,7 +5,7 @@ const postController = require("./posts.controller");
 postRouter.get("/posts", postController.getAll);
 postRouter.get("/posts/:slug", postController.getBySlug);
 postRouter.post("/post", postController.create);
-postRouter.put("/posts/:id", postController.update);
+postRouter.put("/posts/:slug", postController.update);
 postRouter.delete("/posts/:id", postController.delete);
 
 module.exports = postRouter;
